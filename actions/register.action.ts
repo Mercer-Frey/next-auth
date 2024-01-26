7bba2350b09a83b872d29990597a980f527474df
Mercer-frey <deadharmony6969@gmail.com>
2024-01-26 21:20:44 +0200

    register functionality

--- actions/register.action.ts
+++ actions/register.action.ts
@@ -1,11 +1,33 @@
 'use server'
 
+import bcrypt from 'bcrypt'
 import { RegisterSchema, RegisterSchemaInfer } from "@/schemas/validations/register.schema";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: RegisterSchemaInfer) => {
 	const validateFields = RegisterSchema.safeParse(values)
 	
 	if (!validateFields.success) return {error: 'Invalid fields!'}
 	
-	return {success: 'Email sent!'}
+	const {name, email, password} = validateFields.data
+	const hashedPassword = await bcrypt.hash(password, 10)
+	
+	const existingUser = await getUserByEmail(email)
+	
+	if (existingUser) {
+		return {error: 'Email already in use!'}
+	}
+	
+	await db
+		.user
+		.create({
+			data: {
+				name,
+				email,
+				password: hashedPassword,
+			},
+		})
+	
+	return {success: 'User created!'}
 }
