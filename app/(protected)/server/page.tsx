7e19918e59cb0774e3bd272bcfc96d1792e6abf6
Mercer-frey <deadharmony6969@gmail.com>
2024-01-30 20:29:23 +0200

    server page is done

--- app/(protected)/server/page.tsx
+++ app/(protected)/server/page.tsx
@@ -1,15 +1,15 @@
-import { currentUser } from "@/lib/auth";
-import { UserInfo } from "@/components/user-info";
+import { currentUser } from '@/lib/auth'
+import { UserInfo } from '@/components/user-info'
 
-const ServerPage = async () => {
-	const user = await currentUser();
+const Page = async () => {
+	const user = await currentUser()
 	
 	return (
 		<UserInfo
 			label="💻 Server component"
 			user={ user }
 		/>
-	);
+	)
 }
 
-export default ServerPage;
+export default Page
