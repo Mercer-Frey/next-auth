7eacf4e41d14aa5540e4ae80536e1d10e6928f15
Mercer-frey <deadharmony6969@gmail.com>
2024-01-30 20:29:30 +0200

    client page is done

--- app/(protected)/client/page.tsx
+++ app/(protected)/client/page.tsx
@@ -1,10 +1,17 @@
 'use client'
 
+import { UserInfo } from '@/components/user-info'
+import { useCurrentUser } from '@/hooks/use-current-user'
+
 const Page = () => {
+	const user = useCurrentUser()
 	
 	return (
-		<></>
-	);
+		<UserInfo
+			label="📱 Client component"
+			user={ user }
+		/>
+	)
 }
 
-export default Page;
+export default Page
